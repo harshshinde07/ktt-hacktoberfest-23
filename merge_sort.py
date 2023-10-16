24cf4915b49e2554ac0a2a32ae5bf009d6b856dc
shubham sutar <68117385+shubham-sutar@users.noreply.github.com>
2023-10-16 09:29:12 +0530

    Update merge_sort.py
    changes variable uppercase to lowercase.

--- merge_sort.py
+++ merge_sort.py
@@ -6,35 +6,35 @@ def mergeSort(array):
 
         #  r is the point where the array is divided into two subarrays
         r = len(array)//2
-        L = array[:r]
-        M = array[r:]
+        l= array[:r]
+        m = array[r:]
 
         # Sort the two halves
-        mergeSort(L)
-        mergeSort(M)
+        mergeSort(l)
+        mergeSort(m)
 
         i = j = k = 0
 
         # Until we reach either end of either L or M, pick larger among
         # elements L and M and place them in the correct position at A[p..r]
-        while i < len(L) and j < len(M):
-            if L[i] < M[j]:
-                array[k] = L[i]
+        while i < len(l) and j < len(m):
+            if l[i] < m[j]:
+                array[k] = l[i]
                 i += 1
             else:
-                array[k] = M[j]
+                array[k] = m[j]
                 j += 1
             k += 1
 
         # When we run out of elements in either L or M,
         # pick up the remaining elements and put in A[p..r]
-        while i < len(L):
-            array[k] = L[i]
+        while i < len(l):
+            array[k] = l[i]
             i += 1
             k += 1
 
-        while j < len(M):
-            array[k] = M[j]
+        while j < len(m):
+            array[k] = m[j]
             j += 1
             k += 1
 
