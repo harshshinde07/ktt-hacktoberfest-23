5a25417d810987b2ab94437a1589d93b0783bbcb
Lahu S. Andhale <111334418+Lahu19@users.noreply.github.com>
2023-10-22 14:45:58 +0530

    I have added the patterns

---
+++ Patterns/NumberedPramid.java
@@ -0,0 +1,30 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class NumberedPramid {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the size Pyramid : ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+/*
+ * Output:
+Enter the size Pyramid : 6
+     1
+    2 2
+   3 3 3
+  4 4 4 4
+ 5 5 5 5 5
+6 6 6 6 6 6
+ */
